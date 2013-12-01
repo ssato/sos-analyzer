7991613e268ab72621787d606655e68a5c490ae9
Satoru SATOH <satoru.satoh@gmail.com>
2013-12-01 09:30:35 +0900

    add files for packaging

---
+++ setup.py
@@ -0,0 +1,92 @@
+from distutils.core import setup, Command
+from glob import glob
+
+import os.path
+import os
+import sys
+
+curdir = os.getcwd()
+sys.path.append(curdir)
+
+PACKAGE = "sos-analyzer"
+VERSION = "0.0.1"
+
+# For daily snapshot versioning mode:
+if os.environ.get("_SNAPSHOT_BUILD", None) is not None:
+    import datetime
+    VERSION = VERSION + datetime.datetime.now().strftime(".%Y%m%d")
+
+
+def list_files(tdir):
+    return [f for f in glob(os.path.join(tdir, '*')) if os.path.isfile(f)]
+
+
+data_files = [# (destdir, list_files(srcdir),
+             ]
+
+
+class SrpmCommand(Command):
+
+    user_options = []
+
+    build_stage = "s"
+    cmd_fmt = """rpmbuild -b%(build_stage)s \
+        --define \"_topdir %(rpmdir)s\" \
+        --define \"_sourcedir %(rpmdir)s\" \
+        --define \"_buildroot %(BUILDROOT)s\" \
+        %(rpmspec)s
+    """
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        self.run_command('sdist')
+        self.build_rpm()
+
+    def build_rpm(self):
+        params = dict()
+
+        params["build_stage"] = self.build_stage
+        rpmdir = params["rpmdir"] = os.path.join(os.path.abspath(os.curdir),
+                                                 "dist")
+        rpmspec = params["rpmspec"] = \
+            os.path.join(rpmdir, "../%s.spec" % PACKAGE)
+
+        for subdir in ("SRPMS", "RPMS", "BUILD", "BUILDROOT"):
+            sdir = params[subdir] = os.path.join(rpmdir, subdir)
+
+            if not os.path.exists(sdir):
+                os.makedirs(sdir, 0755)
+
+        c = open(rpmspec + ".in").read()
+        open(rpmspec, "w").write(c.replace("@VERSION@", VERSION))
+
+        os.system(self.cmd_fmt % params)
+
+
+class RpmCommand(SrpmCommand):
+
+    build_stage = "b"
+
+
+setup(name=PACKAGE,
+    version=VERSION,
+    description="A tool to scan and analyze data collected by sosreport",
+    author="Satoru SATOH",
+    author_email="ssato@redhat.com",
+    license="GPLv3+",
+    url="https://github.com/ssato/sos-analyzer",
+    packages=["sos_analyzer",
+              "sos_analyzer.tests",
+              "sos_analyzer.plugins",
+             ],
+    scripts=glob("tools/*"),
+    data_files=data_files,
+    cmdclass={"srpm": SrpmCommand, "rpm":  RpmCommand, },
+)
+
+# vim:sw=4:ts=4:et:
