c2f55e55dca787319a7db86d0d7a818c3d9a8685
Satoru SATOH <satoru.satoh@gmail.com>
2013-12-02 19:31:35 +0900

    add 'chkconfig' scanner and its test cases and test data

---
+++ sos_analyzer/scanners/chkconfig.py
@@ -0,0 +1,78 @@
+#
+# Author: Satoru SATOH <ssato redhat.com>
+# License: GPLv3+
+#
+from sos_analyzer.globals import LOGGER as logging
+
+import sos_analyzer.scanners.base as SSB
+import re
+
+
+"""chkconfig format:
+atd             0:off   1:off   2:off   3:on    4:on    5:on    6:off
+auditd          0:off   1:off   2:on    3:on    4:on    5:on    6:off
+...
+xinetd based services:
+        chargen-dgram:  off
+        chargen-stream: off
+        daytime-dgram:  off
+"""
+
+INPUT = "chkconfig"
+STATES = (IN_SVCS, AT_XINETD_SVCS_START, IN_XINETD_SVCS) = (0, 1, 2)
+
+REG_0 = r"(?P<service>[a-zA-Z]\S+)" + \
+        r"\s+0:(on|off)\s+1:(on|off)\s+2:(on|off)\s+3:(on|off)" + \
+        r"\s+4:(on|off)\s+5:(on|off)\s+6:(on|off)"
+REG_1 = r"^ +(?P<service>[^:]+):\s+(?P<status>on|off)"
+
+CONF = dict(initial_state=IN_SVCS,
+            ignore_empty_lines=1,
+            patterns=dict(xinetd_svcs_start="^xinetd [^:]+:",
+                          xinetd_svc=REG_1,
+                          svc=REG_0))
+
+
+class Scanner(SSB.BaseScanner):
+
+    name = input_name = INPUT
+    conf = CONF
+
+    def _update_state(self, state, line, i):
+        if self.match("xinetd_svcs_start", line):
+            return AT_XINETD_SVCS_START
+
+        if state == AT_XINETD_SVCS_START:
+            return IN_XINETD_SVCS  # Next state
+        else:
+            return self.getconf("initial_state")
+
+    def parse_impl(self, state, line, i, *args, **kwargs):
+        """
+        :param state: A dict object represents internal state
+        :param line: Content of the line
+        :param i: Line number in the input file
+        :return: A dict instance of parsed result
+        """
+        if state == IN_SVCS:
+            m = self.match("svc", line)
+            if m:
+                t = m.groups()
+                return dict(service=t[0], status=t[1:])
+            else:
+                e = "Not a line for normal services? l=%s, lno=%d" % (line, i)
+                logging.warn(e)
+
+        elif state == IN_XINETD_SVCS:
+            m = self.match("xinetd_svc", line)
+            if m:
+                return m.groupdict()
+            else:
+                e = "Not a line for xinetd services? l=%s, lno=%d" % (line, i)
+                logging.warn(e)
+        else:
+            pass
+
+        return None
+
+# vim:sw=4:ts=4:et:
---
+++ sos_analyzer/scanners/tests/chkconfig.py
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2013 Satoru SATOH <ssato @ redhat.com>
+# License: GPLv3+
+#
+import sos_analyzer.tests.common as C
+import sos_analyzer.scanners.chkconfig as TT
+import os.path
+import random
+import unittest
+
+
+CURDIR = os.path.dirname(__file__)
+INPUT_0 = os.path.join(CURDIR, "rhel-6-client-1.chkconfig.txt")
+
+
+def get_random_line(input=INPUT_0, start=0, end=None):
+    ls = open(input).read().splitlines()
+    if end is None:
+        end = len(ls)
+
+    while True:
+        l = ls[random.randint(start, end)]
+        if l:
+            return l
+
+
+class Test_00_Scanner(unittest.TestCase):
+
+    def setUp(self):
+        self.datadir = CURDIR
+        self.scanner = TT.Scanner(self.datadir, INPUT_0)
+
+    def test_10_regexes(self):
+        # FIXME: Hard-coded line numbers.
+        line = get_random_line(end=35)
+        #TT.logging.warn("line=" + line)
+        self.assertTrue(self.scanner.match("svc", line), line)
+
+        line = get_random_line(start=41, end=52)
+        self.assertFalse(self.scanner.match("svc", line), line)
+        self.assertTrue(self.scanner.match("xinetd_svc", line), line)
+
+    def test_50_scan_file(self):
+        results = self.scanner.scan_file()
+        print >> open('/tmp/result.txt', 'w').write(str(results))
+
+        self.assertTrue(results)
+
+# vim:sw=4:ts=4:et:
