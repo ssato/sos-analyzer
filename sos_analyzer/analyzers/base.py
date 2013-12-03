2bd79f16036373a0db9d7d9e39c9f9a44b764feb
Satoru SATOH <satoru.satoh@gmail.com>
2013-12-03 10:04:37 +0900

    add analyzer base class

---
+++ sos_analyzer/analyzers/base.py
@@ -0,0 +1,36 @@
+#
+# Base class for analyzers.
+#
+# Author: Satoru SATOH <ssato redhat.com>
+# License: GPLv3+
+#
+from sos_analyzer.globals import LOGGER as logging, \
+    ANALYZER_RESULTS_SUBDIR as SUBDIR
+from sos_analyzer.scanners.base import BaseScanner
+
+import os.path
+
+
+NULL_DICT = dict()
+
+
+class BaseAnalyzer(object):
+
+    name = "base"
+    conf = NULL_DICT
+
+    def __init__(self, workdir, datadir, name=None, conf=None, subdir=SUBDIR):
+        self.datadir = datadir
+
+        if name is not None:
+            self.name = name
+
+        if conf is not None and isinstance(conf, dict):
+            self.conf = conf
+
+        self.result_dir = os.path.join(workdir, subdir, self.name)
+
+    def analyze(self, *args, **kwargs):
+        pass    
+
+# vim:sw=4:ts=4:et:
