81b809ac9fdce9ea0fe7972e3b4f055ffac6093f
Satoru SATOH <satoru.satoh@gmail.com>
2013-12-05 22:50:01 +0900

    add df example outputs

--- sos_analyzer/scanner/df.py
+++ sos_analyzer/scanner/df.py
@@ -9,23 +9,23 @@ import re
 
 
 """df output formats:
-
 1:
 Filesystem           1K-ブロック    使用   使用可 使用% マウント位置
-/dev/sda3              2030768    353548   1572396  19% /
-proc                         0         0         0   -  /proc
-sysfs                        0         0         0   -  /sys
-devpts                       0         0         0   -  /dev/pts
-/dev/sdc1              8123168    526360   7177516   7% /var
-/dev/sda8              2948908     71748   2724948   3% /tmp
-/dev/sda7              3050060    249668   2642960   9% /opt
-/dev/sda6              4061540   2909064    942832  76% /usr
-/dev/sda5              4870688    141028   4478248   4% /var/crash
-/dev/sdb1              1011448     17928    941312   2% /home
-/dev/sda1               303344     20718    266965   8% /boot
-tmpfs                  1956360         0   1956360   0% /dev/shm
-none                         0         0         0   -  /proc/sys/fs/binfmt_misc
-sunrpc                       0         0         0   -  /var/lib/nfs/rpc_pipefs
+/dev/mapper/vg_rhel6client1-lv_root
+                       4684748   2668868   1777904  61% /
+tmpfs                   510292         0    510292   0% /dev/shm
+/dev/vda1               495844     73591    396653  16% /boot
+
+2:
+ファイルシス                 1K-ブロック       使用    使用可 使用% マウント位置
+devtmpfs                         8164368          0   8164368    0% /dev
+tmpfs                            8209916          0   8209916    0% /dev/shm
+tmpfs                            8209916      21596   8188320    1% /run
+tmpfs                            8209916          0   8209916    0% /sys/fs/cgroup
+/dev/mapper/vg0-lv_root       1920455616  836477948 986401120   46% /
+tmpfs                            8209916          0   8209916    0% /tmp
+/dev/sda1                         194241     101223     78682   57% /boot
+/dev/mapper/vg0_data-lv_data  1952559608 1187673728 764885880   61% /srv/data
 """
 
 FS_RE = r"^(?P<filesystem>\S+)\s+(?P<max_blocks>\d+)\s+" + \
