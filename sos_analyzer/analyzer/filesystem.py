ac0e34b449b7583832cc371d1a4956c35462e98e
Satoru SATOH <satoru.satoh@gmail.com>
2013-12-06 21:09:06 +0900

    add an experimental analyzer for filesystems

---
+++ sos_analyzer/analyzer/filesystem.py
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2013 Red Hat, Inc.
+# Author: Satoru SATOH <ssato redhat.com>
+# License: GPLv3+
+#
+from sos_analyzer.globals import LOGGER as logging
+
+import sos_analyzer.analyzer.base as Base
+import re
+
+
+def find_close_to_full_filesystems(workdir, limit=80, input="df"):
+    """
+    :see: ``sos_analyzer.scanner.df``
+    """
+    data = Base.load_scanned_data(workdir, input)
+    if data:
+        for d in data:
+            if int(d.get("used_rate", 0)) > limit:
+                yield d
+
+
+class Analyzer(Base.Analyzer):
+
+    name = "filesystem"
+
+    def analyze(self, *args, **kwargs):
+        cfses = list(find_close_to_full_filesystems(self.workdir))
+
+        return dict(close_to_full_filesystems=cfses)
+
+# vim:sw=4:ts=4:et:
