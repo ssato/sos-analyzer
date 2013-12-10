18a68d22d788d4e522b654162504322712ce01b8
Satoru SATOH <satoru.satoh@gmail.com>
2013-12-10 14:42:26 +0900

    add scanner for /etc/sysconfig/i18n

---
+++ sos_analyzer/scanner/etc_sysconfig_i18n.py
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2013 Red Hat, Inc.
+# Author: Satoru SATOH <ssato redhat.com>
+# License: GPLv3+
+#
+from sos_analyzer.globals import LOGGER as logging
+
+import sos_analyzer.scanner.base as SSB
+
+
+"""/etc/sysconfig/i18n formats:
+
+LANG="en_US.UTF-8"
+SYSFONT="latarcyrheb-sun16"
+"""
+
+class Scanner(SSB.SinglePatternScanner):
+
+    name = input_name = "df"
+    pattern = r'^(?P<option>[^=]+)="?(?P<value>\S+)"?.*$'
+
+# vim:sw=4:ts=4:et:
