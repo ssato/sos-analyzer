55723c1c9620686d95c2b293e4d998b391fbaae5
Satoru SATOH <satoru.satoh@gmail.com>
2013-12-10 13:26:02 +0900

    check if entries that device name is specified in /etc/fstab instead of label, UUIDs, etc.

--- sos_analyzer/analyzer/filesystem.py
+++ sos_analyzer/analyzer/filesystem.py
@@ -21,13 +21,31 @@ def find_close_to_full_filesystems(workdir, limit=80, input="df.json"):
                 yield d
 
 
+NG_DEVS_RE = re.compile(r"^/dev/(?P<drive>h|s|v)d[a-z]\S+")
+
+
+def find_ng_dev_specs(workdir, ng_devs_re=NG_DEVS_RE, input="etc/fstab.json"):
+    """
+    :see: ``sos_analyzer.scanner.etc_fstab``
+    """
+    data = Base.load_scanned_data(workdir, input)
+    if data:
+        for d in data:
+            dev=d["device"]
+            m = ng_devs_re.match(dev)
+            if m:
+                yield d
+
+
 class Analyzer(Base.Analyzer):
 
     name = "filesystem"
 
     def analyze(self, *args, **kwargs):
         cfses = list(find_close_to_full_filesystems(self.workdir))
+        ng_devs = list(find_ng_dev_specs(self.workdir))
 
-        return dict(close_to_full_filesystems=cfses)
+        return dict(close_to_full_filesystems=cfses,
+                    device_names_specified_in_fstab=ng_devs)
 
 # vim:sw=4:ts=4:et:
