2c9e2f82d271e8a3b12b237b6b76b5371eebc6ec
Satoru SATOH <satoru.satoh@gmail.com>
2014-07-07 19:57:47 +0900

    enabled pyflake8 checks

--- aux/runtest.sh
+++ aux/runtest.sh
@@ -8,10 +8,22 @@ if `env | grep -q 'WITH_COVERAGE' 2>/dev/null`; then
     coverage_opts="--with-coverage --cover-tests --cover-inclusive"
 fi
 
+function _pylint () {
+    pylint --rcfile=$curdir/pylintrc --errors-only $@
+}
+
 which pep8 2>&1 > /dev/null && check_with_pep8=1 || check_with_pep8=0
+which flake8 2>&1 > /dev/null && { check_with_pep8=0; check_with_flake8=1;} || check_with_flake8=0
+which pylint 2>&1 > /dev/null && check_with_pylint=1 || check_with_pylint=0
 
 if test $# -gt 0; then
-    test $check_with_pep8 = 1 && (for x in $@; do pep8 ${x%%:*}; done) || :
+    if test $check_with_pep8 = 1; then
+        for x in $@; do pep8 ${x%%:*}; done
+    fi
+    test $check_with_flake8 = 1 && flake8 $@
+    if test $check_with_pylint = 1; then
+        for x in $@; do _pylint ${x%%:*}; done
+    fi
     PYTHONPATH=$topdir nosetests -c $curdir/nose.cfg ${coverage_opts} $@
 else
     # Find out python package dir and run tests for .py files under it.
@@ -21,7 +33,8 @@ else
 
             for f in $(find ${pypkgdir} -name '*.py'); do
                 echo "[Info] Check $f..."
-                test $check_with_pep8 = 1 && pep8 $f || :
+                if test $check_with_pep8 = 1; then pep8 $f; fi
+                if test $check_with_pylint = 1; then _pylint $f; fi
                 PYTHONPATH=$topdir nosetests -c $curdir/nose.cfg \
                         ${coverage_opts} $f
             done
@@ -29,4 +42,7 @@ else
             break
         fi
     done
+    test $check_with_flake8 = 1 && flake8 ${topdir}
 fi
+
+# vim:sw=4:ts=4:et:
